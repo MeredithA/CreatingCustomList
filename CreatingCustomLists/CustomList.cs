de14cfbf6ca5dad395aec34982ee2349f07ec4a8
MeredithA <meredith.albrecht00@gmail.com>
2018-03-01 14:46:53 -0600

    initial push

---
+++ CreatingCustomLists/CustomList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingCustomLists
+{
+    class CustomList
+    {
+        //member variable (HAS A)
+        Array firstArray = new Array[5];
+        Array secondArray = new Array[10];
+
+
+
+        //constructor (method in the class which gets executed when its object is created)
+
+
+
+        //member methods(CAN DO)
+
+
+
+
+
+
+
+        //a count int - how many there are
+        //objects
+
+        //make an array - to add copy original array and then add them together
+
+        //add object method
+
+        //remove object method
+
+        // loop through list method
+        //index
+
+        //??overrice ToString method
+        //create ToString method and override
+        //create + & - opperator & overload 
+
+        //count elements in list class
+
+        //11 linking lists together like a zipper
+
+    }
+}
---
+++ CreatingCustomLists/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingCustomLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
